76f0268f0d98e8c1a4b07b3431fea205e6b4bcf1
Disha-L12 <disha1222005@gmail.com>
2026-03-08 16:36:07 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("-profile");
+        options.addArguments("/home/vboxuser/snap/firefox/common/.mozilla/firefox/vm8oz4h9.seleniumprofile");
+
+        WebDriver driver = new FirefoxDriver(options);
+
+        // Open SauceDemo
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(3000);
+
+        // Navigate to Practice Test site
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        Thread.sleep(2000);
+
+        // Login
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
