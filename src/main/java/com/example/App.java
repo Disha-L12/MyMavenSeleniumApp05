1f11e3226ff19e00b74437eee74ecf6a4116c7c1
Disha L <disha1222005@gmail.com>
2026-04-16 16:08:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,6 +10,13 @@ public class App {
     public static void main(String[] args) throws InterruptedException {
 
         FirefoxOptions options = new FirefoxOptions();
+        
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
         options.addArguments("-profile");
         options.addArguments("/home/vboxuser/snap/firefox/common/.mozilla/firefox/vm8oz4h9.seleniumprofile");
 
